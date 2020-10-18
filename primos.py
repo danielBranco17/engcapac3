f6b27d052b9fe685f8f3c78974ed1576e290e507
Daniel <do29137@gmail.com>
2020-10-18 15:17:27 -0300

    AC3

--- primos.py
+++ primos.py
@@ -30,5 +30,5 @@ def primos():
     return primos
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
