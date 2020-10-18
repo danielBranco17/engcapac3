9dfddc77bda80b2059852550266ffc5c5fd15bdf
Daniel <do29137@gmail.com>
2020-10-18 14:58:32 -0300

    AC3

---
+++ primos.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/a')
+def primos():
+    limite = 100
+
+    a = 1
+    b = 1
+    n = 3
+
+    primos = '2,'
+
+    while b < limite:
+        ehprimo = 1
+        for i in range(2, n):
+            if n % i == 0:
+                ehprimo = 0
+                break
+
+        if(ehprimo):
+            primos = primos + str(n) + '.'
+            b += 1
+            if(b % 10 == 0):
+                primos = primos + '<br>'
+        n += 1
+
+    return primos
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6000))
+    app.run(host='0.0.0.0', port=port)
